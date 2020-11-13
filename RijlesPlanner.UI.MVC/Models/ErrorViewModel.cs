cbc9c67df82cc3b5a6adb7361d15afd2827257eb
Ryan Houben <ryanhouben1998@outlook.com>
2020-11-13 10:32:32 +0100

    - Added the UI layer.

---
+++ RijlesPlanner.UI.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RijlesPlanner.UI.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
